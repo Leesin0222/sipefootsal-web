52234f78d0b912424ba9079429714ccfecf22338
leeyongjin <dragon02229@naver.com>
2026-02-13 10:37:43 +0900

    add github ci workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "/",
+  base: isGitHubPages ? "/sipefootsal-web/" : "/",
 });
